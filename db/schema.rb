3b92c7775136e50143242c6bcb8d0aa199bc03f3
Dexter Go <d.go@neura.edu.au>
2016-09-20 16:16:30 +1000

    Add styling.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919151649) do
+ActiveRecord::Schema.define(version: 20160920043911) do
 
   create_table "notes", force: :cascade do |t|
     t.integer  "participant_id"
