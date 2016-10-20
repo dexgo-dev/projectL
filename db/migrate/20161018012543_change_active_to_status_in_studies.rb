5b74ee829aeb52ba0ad62a605b8d0b09aed96c92
Dexter Go <d.go@neura.edu.au>
2016-10-20 15:24:25 +1100

    Remove this migration

--- db/migrate/20161018012543_change_active_to_status_in_studies.rb
+++
@@ -1,6 +0,0 @@
-class ChangeActiveToStatusInStudies < ActiveRecord::Migration[5.0]
-  def change
-  	rename_column :studies, :active, :status
-  	change_column :studies, :status, :integer
-  end
-end
