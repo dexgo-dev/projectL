0c2f6dd31b223b65057d0016c954c14c753d10a2
Dexter Go <d.go@neura.edu.au>
2016-10-20 15:28:02 +1100

    Fix email address.

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -11,6 +11,6 @@ class NotifierMailer < ApplicationMailer
     @participant = @logger_note.participant
 
     # attachments['n.pdf'] = File.read("#{Rails.root}/public/note.pdf")
-    mail to: @user.email, subject: 'Your note notification for participant: ' + @participant.name, cc: 'd.go@neura.edu.au', bcc: 'dexterfgo@gmail.com', reply_to: d.go@neura.edu.au
+    mail to: @user.email, subject: 'Your note notification for participant: ' + @participant.name, cc: 'd.go@neura.edu.au', bcc: 'dexterfgo@gmail.com', reply_to: 'd.go@neura.edu.au'
   end
 end
