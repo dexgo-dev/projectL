9fd722fc7cb93840d43c8295b88e7b29e261b457
Dexter Go <d.go@neura.edu.au>
2016-11-15 17:06:32 +1100

    Check if column user_name exists before removing it.

--- db/migrate/20161006023701_remove_username_from_users.rb
+++ db/migrate/20161006023701_remove_username_from_users.rb
@@ -1,5 +1,7 @@
 class RemoveUsernameFromUsers < ActiveRecord::Migration[5.0]
   def change
-  	remove_column :users, :user_name
+  	if column_exists? :users, :user_name
+  		remove_column :users, :user_name
+  	end
   end
 end
