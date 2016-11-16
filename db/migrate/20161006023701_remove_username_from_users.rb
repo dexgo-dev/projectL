50be9668b0a9c712d4b5675f89a3c183a5ce66b8
Dexter Go <d.go@neura.edu.au>
2016-11-16 14:48:39 +1100

    Changes to migration and deploy settings.

--- db/migrate/20161006023701_remove_username_from_users.rb
+++ db/migrate/20161006023701_remove_username_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveUsernameFromUsers < ActiveRecord::Migration[5.0]
   def change
-  	if column_exists? :users, :user_name
-  		remove_column :users, :user_name
+  	if column_exists? :users, :username
+  		remove_column :users, :username
   	end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 t = Team.create(name:'No Team', description:'If you are not sure.')
-User.create(email:'d.go@neura.edu.au', password:'password', full_name:'First User', contact_number:'0', team_id: t.id, isApproved: true, isAdmin: true, isActive: true, isBlinded: false, isSupervisor: false)
+User.create(email:'fbirc-dev@neura.edu.au', password:'password', full_name:'First User', contact_number:'0', team_id: t.id, isApproved: true, isAdmin: true, isActive: true, isBlinded: false, isSupervisor: false)
 
 s = Study.create(study_name:'No Study', description:'All participants are to be moved out of this study except for the participant called ToDo which is a dummy participant')
-p = Participant.create(name:'ToDo', gender:3, date_of_birth:Date.today, email:'d.go@neura.edu.au', study_id: s.id, active: true)
+p = Participant.create(name:'ToDo', gender:3, date_of_birth:Date.today, email:'fbirc-dev@neura.edu.au', study_id: s.id, active: true)
