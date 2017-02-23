99b32e255d221641e1cd472a853a3663a1c493ee
Dexter Go <d.go@neura.edu.au>
2017-02-23 13:27:30 +1100

    Change other instances where "name" is referenced.

--- app/mailers/notifier_mailer.rb
+++ app/mailers/notifier_mailer.rb
@@ -11,6 +11,6 @@ class NotifierMailer < ApplicationMailer
     @participant = @logger_note.participant
 
     # attachments['n.pdf'] = File.read("#{Rails.root}/public/note.pdf")
-    mail to: @user.email, subject: 'Your note notification for participant: ' + @participant.name, cc: 'fbirc-dev@neura.edu.au', reply_to: 'fbirc-dev@neura.edu.au'
+    mail to: @user.email, subject: 'Your note notification for participant: ' + @participant.first_name + ' ' + @participant.surname, cc: 'fbirc-dev@neura.edu.au', reply_to: 'fbirc-dev@neura.edu.au'
   end
 end
--- test/controllers/participants_controller_test.rb
+++ test/controllers/participants_controller_test.rb
@@ -17,7 +17,7 @@ class ParticipantsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create participant" do
     assert_difference('Participant.count') do
-      post participants_url, params: { participant: { active: @participant.active, contact_number: @participant.contact_number, date_of_birth: @participant.date_of_birth, email: @participant.email, gender: @participant.gender, home_address: @participant.home_address, name: @participant.name, study_id: @participant.study_id } }
+      post participants_url, params: { participant: { status: @participant.status, primary_contact_number: @participant.primary_contact_number, date_of_birth: @participant.date_of_birth, email: @participant.email, gender: @participant.gender, home_address: @participant.home_address, first_name: @participant.first_name, surname: @participant.surname, study_id: @participant.study_id } }
     end
 
     assert_redirected_to participant_url(Participant.last)
@@ -34,7 +34,7 @@ class ParticipantsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update participant" do
-    patch participant_url(@participant), params: { participant: { active: @participant.active, contact_number: @participant.contact_number, date_of_birth: @participant.date_of_birth, email: @participant.email, gender: @participant.gender, home_address: @participant.home_address, name: @participant.name, study_id: @participant.study_id } }
+    patch participant_url(@participant), params: { participant: { status: @participant.status, primary_contact_number: @participant.primary_contact_number, date_of_birth: @participant.date_of_birth, email: @participant.email, gender: @participant.gender, primary_home_address: @participant.primary_home_address, first_name: @participant.first_name, surname: @participant.surname, study_id: @participant.study_id } }
     assert_redirected_to participant_url(@participant)
   end
 
