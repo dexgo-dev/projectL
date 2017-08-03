93b2647d259b1276b1530a03b39b4229b6088ab0
Dex Go <dexterfgo@gmail.com>
2017-08-04 00:55:57 +1000

    Precompile assets.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,15 +23,12 @@ Rails.application.configure do
   config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  # Enable assets
+  config.assets.enabled = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-  # Bower asset paths
-  root.join('vendor', 'assets', 'components').to_s.tap do |bower_path|
-    config.sass.load_paths << bower_path
-    config.assets.paths << bower_path
-  end
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
