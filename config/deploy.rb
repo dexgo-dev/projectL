5c48a7af3e254060c5b92c64ce3c27c2c8c58b38
Dex Go <dexterfgo@gmail.com>
2017-08-04 18:51:50 +1000

    Remove bin again from linked dirs.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,10 +14,10 @@ set :branch, :aws
 set :deploy_to, '/home/deploy/loggr'
 
 set :pty, true
-# set :bundle_binstubs, nil
+set :bundle_binstubs, nil
 set :linked_files, %w{config/database.yml config/application.yml}
-# set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
+# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
 
